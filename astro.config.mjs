7ae184339c78a8a0c0db2ef6f76ec5a75bfb5406
AxelSparta <axelnicolassparta@gmail.com>
2026-04-09 07:45:13 -0300

    fix: Update routing configuration for default locale, adjust language paths, and enhance index page layout with new components

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,8 @@ export default defineConfig({
     defaultLocale: 'es', // idioma predeterminado
     locales: ['en', 'es'], // idiomas soportados
     routing: {
-      prefixDefaultLocale: true, // colocar el prefijo del idioma para el idioma predeterminado, en caso de que sea false los archivos del idioma predeterminado estarán en la raíz
-      redirectToDefaultLocale: true // redirigir a la versión con el prefijo del idioma predeterminado (no es necesario si prefijamos el idioma predeterminado)
+      prefixDefaultLocale: false, // colocar el prefijo del idioma para el idioma predeterminado, en caso de que sea false los archivos del idioma predeterminado estarán en la raíz
+      redirectToDefaultLocale: false // redirigir a la versión con el prefijo del idioma predeterminado (no es necesario si prefijamos el idioma predeterminado)
     }
   }
 });
